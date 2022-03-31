6362b6ea2bc9847a37084ab2e712b8653ecb647e
lorenzozanella <ldphyandrus@gmail.com>
2022-03-31 09:19:44 -0300

    Nadzoru to FBE converter was completed

--- function_block/converter.py
+++ function_block/converter.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python
 
+from function_block import *
+
 class ECC():
     def __init__(self, **kwargs):
         self.states = set()
         self.events = set() # list [event, BOOL(0 = not controllable, 1 = controllable)]
         self.connections = dict() # a single connection will be a tuple [state1, state2] == state1 -> state2
+        self.initial_state = None
 
     def connect_states(self, state1, state2, event):
         self.connections[event] = (state1, state2)
 
-    def run_ecc(self, initial_state):
-        for event in self.connections.keys():
-            if start in self.connections[event] and event == True:
-                start = self.connections[event][1]
-                self.run_ecc(start)
 
-
-def NZRO_create_ecc(xml, name):
+def NZRO_create_ecc(xml):
     name = ECC()
     parse_lines = open(xml).readlines()
     read_mode = None
@@ -32,7 +29,10 @@ def NZRO_create_ecc(xml, name):
     # erro se name = '<events>', '<states>' ou 'transitions'
             if read_mode == 'state':
                 if word[0:4] == "name":
-                    name.states.add(word[6:-1])	
+                    state_iq = word[6:-1]
+                    name.states.add(state_iq)	
+                if word[0:13] == "initial_state":
+                    name.initial_state = state_iq
 
             elif read_mode == 'event':
                 if word[0:4] == 'name':
@@ -61,17 +61,69 @@ def NZRO_create_ecc(xml, name):
 
     return name
 
-def generate_ECC_fb(ECC):
-	fb = Base_Function_Block()
-	
-	fb.add_event('INIT', Event(self, INIT))
-	fb.add_event('INITO', Event(self))
-	fb.add_event('OUT', Event(self))
-	
-	for event in ECC.events:
-		fb.add_event(event[0], Event(self, event[0]))
-	
-	
+def generate_ECC_fb(ECC, INIT=False, REQ = False):
+    fb = Base_Function_Block()
+
+    setattr(fb, 'ECC', ECC)
+
+    fb.add_event('INIT', Event(fb, INIT))
+    fb.add_event('REQ', Event(fb, REQ))
+    fb.add_event('INITO', Event(fb))
+    fb.add_event('OUT', Event(fb))
+
+    for event in ECC.events:
+        fb.add_event(event[0], Event(fb)) # every event starts as False
+
+    for state in ECC.states:
+        fb.add_event(state, Event(fb)) # output event
+
+    def algorithm(self):
+        if self.INIT:
+            self.INITO.active = True	
+            if self.REQ:
+                for event in ECC.connections.keys():
+                    for states in ECC.connections[event]:
+                        if state[0] == fb.ECC.initial_state and getattr(fb, event).active == True:
+                            states[0].active = False
+                            states[1].active = True
+                            fb.ECC.initial_state = states[1]
+        else:
+            self.INITO.active = False
+
+    setattr(fb, 'algorithm', algorithm)
+
+    return fb
+
+def automata_to_fb(automata):
+    canvas = world()
+
+    ECC_fb = generate_ECC_fb(NZRO_create_ecc(automata))
+    canvas.add_function_block(ECC_fb)
+    OR = E_MERGE(EI1=False, EI2=False)
+    canvas.add_function_block(OR)
+
+    for event in ECC_fb.ECC.events:
+
+        locals()["IN_" + event[0]] = IO_READER(INIT=False, REQ=False, QI=True, PARAMS=None, SD_1=None)
+        canvas.add_function_block(locals()["IN_" + event[0]])
+        locals()["IN_" + event[0]].add_variable('IVAL', Variable(locals()["IN_" + event[0]], False)) # creates IVAL variable in IO_READER fb, sets it to False
+
+        locals()["P_" + event[0]] = PERMIT(EI=False, PERMIT = False)
+        canvas.add_function_block(locals()["P_"+event[0]])
+
+        canvas.connect_events(locals()["IN_" + event[0]].CNF, locals()["P_" + event[0]].EI)
+        canvas.connect_variables(locals()["IN_" + event[0]].RD_1, locals()["P_" + event[0]].PERMIT)
+
+        canvas.connect_events(locals()["P_" + event[0]].EO, getattr(ECC_fb, event[0]))
+        OR.add_event("REQ_" + event[0], Event(OR, False))
+        canvas.connect_events(locals()["P_" + event[0]].EO, getattr(OR, 'REQ_' + event[0]))
+        canvas.connect_events(OR.EO, ECC_fb.REQ)
+
+    return canvas
+
+
+
+
 
 
 
--- gui/fbe_main.py
+++ gui/fbe_main.py
@@ -1,121 +1,192 @@
 #!/usr/bin/python
 
-import sys
-
 import gi
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import GLib, Gio, Gtk
+from gi.repository import Gtk, Gdk
 
-MENU_XML = """
-<?xml version="1.0" encoding="UTF-8"?>
-<interface>
-  <menu id="app-menu">
-    <section>
-      <attribute name="label" translatable="yes">File</attribute>
-      <item>
-        <attribute name="action">app.new</attribute>
-        <attribute name="label" translatable="yes">new</attribute>
-      </item>
-       <item>
-        <attribute name="action">app.save</attribute>
-        <attribute name="label" translatable="yes">save</attribute>
-      </item>
-       <item>
-        <attribute name="action">app.save_as</attribute>
-        <attribute name="label" translatable="yes">save as</attribute>
-      </item>
-    </section>
-    <section>
-      <item>
-        <attribute name="action">win.maximize</attribute>
-        <attribute name="label" translatable="yes">Maximize</attribute>
-      </item>
-    </section>
-    <section>
-      <item>
-        <attribute name="action">app.about</attribute>
-        <attribute name="label" translatable="yes">_About</attribute>
-      </item>
-      <item>
-        <attribute name="action">app.quit</attribute>
-        <attribute name="label" translatable="yes">_Quit</attribute>
-        <attribute name="accel">&lt;Primary&gt;q</attribute>
-    </item>
-    </section>
-  </menu>
-</interface>
+UI_INFO = """
+<ui>
+  <menubar name='MenuBar'>
+    <menu action='FileMenu'>
+      <menu action='FileNew'>
+        <menuitem action='FileNewStandard' />
+        <menuitem action='FileNewFoo' />
+        <menuitem action='FileNewGoo' />
+      </menu>
+      <separator />
+      <menuitem action='FileQuit' />
+    </menu>
+    <menu action='EditMenu'>
+      <menuitem action='EditCopy' />
+      <menuitem action='EditPaste' />
+      <menuitem action='EditSomething' />
+    </menu>
+    <menu action='ChoicesMenu'>
+      <menuitem action='ChoiceOne'/>
+      <menuitem action='ChoiceTwo'/>
+      <separator />
+      <menuitem action='ChoiceThree'/>
+    </menu>
+  </menubar>
+  <toolbar name='ToolBar'>
+    <toolitem action='FileNewStandard' />
+    <toolitem action='FileQuit' />
+  </toolbar>
+  <popup name='PopupMenu'>
+    <menuitem action='EditCopy' />
+    <menuitem action='EditPaste' />
+    <menuitem action='EditSomething' />
+  </popup>
+</ui>
 """
 
 
-class AppWindow(Gtk.ApplicationWindow):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class MenuExampleWindow(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="Menu Example")
+
+        self.set_default_size(200, 200)
+
+        action_group = Gtk.ActionGroup(name="my_actions")
+
+        self.add_file_menu_actions(action_group)
+        self.add_edit_menu_actions(action_group)
+        self.add_choices_menu_actions(action_group)
+
+        uimanager = self.create_ui_manager()
+        uimanager.insert_action_group(action_group)
+
+        menubar = uimanager.get_widget("/MenuBar")
+
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        box.pack_start(menubar, False, False, 0)
+
+        toolbar = uimanager.get_widget("/ToolBar")
+        box.pack_start(toolbar, False, False, 0)
+
+        eventbox = Gtk.EventBox()
+        eventbox.connect("button-press-event", self.on_button_press_event)
+        box.pack_start(eventbox, True, True, 0)
+
+        label = Gtk.Label(label="Right-click to see the popup menu.")
+        eventbox.add(label)
+
+        self.popup = uimanager.get_widget("/PopupMenu")
+
+        self.add(box)
+
+    def add_file_menu_actions(self, action_group):
+        action_filemenu = Gtk.Action(name="FileMenu", label="File")
+        action_group.add_action(action_filemenu)
+
+        action_filenewmenu = Gtk.Action(name="FileNew", stock_id=Gtk.STOCK_NEW)
+        action_group.add_action(action_filenewmenu)
+
+        action_new = Gtk.Action(
+            name="FileNewStandard",
+            label="_New",
+            tooltip="Create a new file",
+            stock_id=Gtk.STOCK_NEW,
+        )
+        action_new.connect("activate", self.on_menu_file_new_generic)
+        action_group.add_action_with_accel(action_new, None)
+
+        action_group.add_actions(
+            [
+                (
+                    "FileNewFoo",
+                    None,
+                    "New Foo",
+                    None,
+                    "Create new foo",
+                    self.on_menu_file_new_generic,
+                ),
+                (
+                    "FileNewGoo",
+                    None,
+                    "_New Goo",
+                    None,
+                    "Create new goo",
+                    self.on_menu_file_new_generic,
+                ),
+            ]
+        )
+
+        action_filequit = Gtk.Action(name="FileQuit", stock_id=Gtk.STOCK_QUIT)
+        action_filequit.connect("activate", self.on_menu_file_quit)
+        action_group.add_action(action_filequit)
+
+    def add_edit_menu_actions(self, action_group):
+        action_group.add_actions(
+            [
+                ("EditMenu", None, "Edit"),
+                ("EditCopy", Gtk.STOCK_COPY, None, None, None, self.on_menu_others),
+                ("EditPaste", Gtk.STOCK_PASTE, None, None, None, self.on_menu_others),
+                (
+                    "EditSomething",
+                    None,
+                    "Something",
+                    "<control><alt>S",
+                    None,
+                    self.on_menu_others,
+                ),
+            ]
+        )
+
+    def add_choices_menu_actions(self, action_group):
+        action_group.add_action(Gtk.Action(name="ChoicesMenu", label="Choices"))
+
+        action_group.add_radio_actions(
+            [
+                ("ChoiceOne", None, "One", None, None, 1),
+                ("ChoiceTwo", None, "Two", None, None, 2),
+            ],
+            1,
+            self.on_menu_choices_changed,
+        )
+
+        three = Gtk.ToggleAction(name="ChoiceThree", label="Three")
+        three.connect("toggled", self.on_menu_choices_toggled)
+        action_group.add_action(three)
+
+    def create_ui_manager(self):
+        uimanager = Gtk.UIManager()
+
+        # Throws exception if something went wrong
+        uimanager.add_ui_from_string(UI_INFO)
+
+        # Add the accelerator group to the toplevel window
+        accelgroup = uimanager.get_accel_group()
+        self.add_accel_group(accelgroup)
+        return uimanager
+
+    def on_menu_file_new_generic(self, widget):
+        print("A File|New menu item was selected.")
+
+    def on_menu_file_quit(self, widget):
+        Gtk.main_quit()
+
+    def on_menu_others(self, widget):
+        print("Menu item " + widget.get_name() + " was selected")
+
+    def on_menu_choices_changed(self, widget, current):
+        print(current.get_name() + " was selected.")
+
+    def on_menu_choices_toggled(self, widget):
+        if widget.get_active():
+            print(widget.get_name() + " activated")
+        else:
+            print(widget.get_name() + " deactivated")
+
+    def on_button_press_event(self, widget, event):
+        # Check if right mouse button was preseed
+        if event.type == Gdk.EventType.BUTTON_PRESS and event.button == 3:
+            self.popup.popup(None, None, None, None, event.button, event.time)
+            return True  # event has been handled
 
 
-class Application(Gtk.Application):
-    def __init__(self, *args, **kwargs):
-        super().__init__(
-                *args,
-                application_id="org.example.myapp",
-                **kwargs
-                )
-        self.window = None
-
-    def do_startup(self):
-        Gtk.Application.do_startup(self)
-
-        action = Gio.SimpleAction.new("about", None)
-        action.connect("activate", self.on_about)
-        self.add_action(action)
-
-        action = Gio.SimpleAction.new("quit", None)
-        action.connect("activate", self.on_quit)
-        self.add_action(action)
-
-        action = Gio.SimpleAction.new("new", None)
-        action.connect("activate", self.on_new)
-        self.add_action(action)
-
-        action = Gio.SimpleAction.new("save", None)
-        action.connect("activate", self.on_save)
-        self.add_action(action)
-
-        action = Gio.SimpleAction.new("save_as", None)
-        action.connect("activate", self.on_save_as)
-        self.add_action(action)
-
-        builder = Gtk.Builder.new_from_string(MENU_XML, -1)
-        self.set_app_menu(builder.get_object("app-menu"))
-
-        
-
-    def do_activate(self):
-        # We only allow a single window and raise any existing ones
-        if not self.window:
-            # Windows are associated with the application
-            # when the last one is closed the application shuts down
-            self.window = AppWindow(application=self, title="Main Window")
-
-        self.window.present()
-
-    def on_about(self, action, param):
-        about_dialog = Gtk.AboutDialog(transient_for=self.window, modal=True)
-        about_dialog.present()
-
-    def on_quit(self, action, param):
-        self.quit()
-
-    def on_new(self, action, param):
-        pass
-
-    def on_save(self, action, param):
-        pass
-
-    def on_save_as(self, action, param):
-        pass
-
-
-if __name__ == "__main__":
-    app = Application()
-    app.run(sys.argv)
+window = MenuExampleWindow()
+window.connect("destroy", Gtk.main_quit)
+window.show_all()
+Gtk.main()
